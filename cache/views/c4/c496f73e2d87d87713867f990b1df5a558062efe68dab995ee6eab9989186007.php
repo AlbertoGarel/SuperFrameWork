16d3e12cdb9e9e6c5b8000c288f3190e08cb9ad7
Xavi <xavi@geekshubs.com>
2019-07-29 10:45:51 +0200

    Doctrine added and create em from users

--- cache/views/c4/c496f73e2d87d87713867f990b1df5a558062efe68dab995ee6eab9989186007.php
+++ cache/views/c4/c496f73e2d87d87713867f990b1df5a558062efe68dab995ee6eab9989186007.php
@@ -12,8 +12,8 @@ use Twig\Sandbox\SecurityNotAllowedFunctionError;
 use Twig\Source;
 use Twig\Template;
 
-/* where.twig.html */
-class __TwigTemplate_07e2289a1e3eec8797576c8bf747b98916301b113c180bb809d49dce89fccd1b extends \Twig\Template
+/* usersView.twig.html */
+class __TwigTemplate_17436f7dc4161ba14545d0c6c57b7f4f79e6254071dc5110b28ae9bffe6bf601 extends \Twig\Template
 {
     private $source;
     private $macros = [];
@@ -38,7 +38,7 @@ class __TwigTemplate_07e2289a1e3eec8797576c8bf747b98916301b113c180bb809d49dce89f
     protected function doDisplay(array $context, array $blocks = [])
     {
         $macros = $this->macros;
-        $this->parent = $this->loadTemplate("template.twig.html", "where.twig.html", 1);
+        $this->parent = $this->loadTemplate("template.twig.html", "usersView.twig.html", 1);
         $this->parent->display($context, array_merge($this->blocks, $blocks));
     }
 
@@ -47,13 +47,13 @@ class __TwigTemplate_07e2289a1e3eec8797576c8bf747b98916301b113c180bb809d49dce89f
     {
         $macros = $this->macros;
         // line 4
-        echo "    <h1>Donde Estamos</h1>
+        echo "    <h1>Hola Users</h1>
 ";
     }
 
     public function getTemplateName()
     {
-        return "where.twig.html";
+        return "usersView.twig.html";
     }
 
     public function isTraitable()
@@ -68,6 +68,6 @@ class __TwigTemplate_07e2289a1e3eec8797576c8bf747b98916301b113c180bb809d49dce89f
 
     public function getSourceContext()
     {
-        return new Source("", "where.twig.html", "/home/xavi/Documentos/COORDINACIÓN/bootcampValenciaMayo19/php/superframework/templates/where.twig.html");
+        return new Source("", "usersView.twig.html", "/home/xavi/Documentos/COORDINACIÓN/bootcampValenciaMayo19/php/superframework/templates/usersView.twig.html");
     }
 }
